65d16d57f24553f40b3b8a23bfa880b7332d48c1
Soren <soren@aethre.co>
2020-01-25 10:50:17 -0800

    Fetch API!

--- js/navigation.js
+++ js/navigation.js
@@ -2,8 +2,12 @@ $.ajaxSetup({
   cache: window.location.hostname === 'localhost'
 })
 
-function load(src) {
-  $('#container').load(src + '.html', _ => {
+function load(src, clear = true) {
+  fetch(src + '.html', { cache: window.location.hostname === 'localhost' ? 'no-store' : 'default' }).then(res => res.text()).then(body => {
+    if (clear) {
+      $('#container').empty()
+    }
+    $('#container').append($(body))
     var url = URI(window.location.href)
     window.location.href = '#'
     var s = {
@@ -13,7 +17,7 @@ function load(src) {
     if (t) s.theme = variant
 
     url.search(s)
-    window.history.replaceState(null, null, url.toString())
+    // window.history.pushState(s, s.path, url.toString())
     $('#brand').attr('src', '/img/logos/brand-' + variant + '.png')
   })
 }
