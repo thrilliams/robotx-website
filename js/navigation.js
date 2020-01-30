786ec1226276f948c9a4add2aa9c12971f8b29f7
Soren <soren@aethre.co>
2020-01-29 20:18:15 -0800

    Commenting and consistency!

--- js/navigation.js
+++ js/navigation.js
@@ -1,43 +1,54 @@
-$.ajaxSetup({
-    cache: window.location.hostname === 'localhost'
-})
-
+// Load a URL into the main container
 function load(src, clear = true) {
-    fetch(src + '.html', { cache: window.location.hostname === 'localhost' ? 'no-store' : 'default' }).then(res => res.text()).then(body => {
-        if (clear) {
-            $('#container').empty()
-        }
-        $('#container').append($(body))
-        var url = URI(window.location.href)
-        window.location.href = '#'
-        var s = {
-            path: src
-        }
+    fetch(src + '.html', {
+        // Only use cache if developing locally
+        cache: window.location.hostname === 'localhost' ? 'no-store' : 'default'
+    }).then(res => res.text()).then(body => {
+        // Usually clean out the main container, sometimes we might need to load multiple pages consecutively
+        if (clear) $('#container').empty();
+        $('#container').append($(body));
 
-        if (t) s.theme = variant
+        // Save path and theme (sometimes)
+        var s = { path: src };
+        if (forceTheme) s.theme = forceTheme;
 
-        url.search(s)
-        window.history.pushState(s, 'Robot X', url.toString())
-        $('#brand').attr('src', '/img/logos/brand-' + variant + '.png')
-    })
+        // Put them in window.location
+        var url = URI(window.location.href);
+        url.search(s);
+        window.history.pushState(s, 'Robot X', url.toString());
+    });
 }
 
-var list = $('#list').find('button')
-$(list.slice(-2)[0]).addClass('last')
+// Round out the bottom of the second to last item in the navbar so the donate button looks byootiful
+var list = $('#list').find('button');
+$(list.slice(-2)[0]).addClass('last');
 
+// Set click event for each button in the navbar
 list.click(e => {
+    // The donate button doesn't have a target page, so we ignore it
     if (e.target.dataset.href) {
-        load(e.target.dataset.href)
-        list.removeClass('active')
-        $(e.target).addClass('active')
-        if ($(e.target).parent()[0] !== $('#robot-menu')[0]) $('#robot-menu').collapse('hide')
-        else $('#robot-menu').collapse('show')
+        // First load the target page
+        load(e.target.dataset.href);
+
+        // Set list as inactive
+        list.removeClass('active');
+
+        // Set button as active and show dropdown if necessary
+        $(e.target).addClass('active');
+        if ($(e.target).parent()[0] !== $('#robot-menu')[0]) $('#robot-menu').collapse('hide');
+        else $('#robot-menu').collapse('show');
     }
-    e.target.blur()
+
+    // Begone, accursed blue outline!
+    e.target.blur();
 })
 
-var path = URI(window.location.href).search(true).path ? unescape(URI(window.location.href).search(true).path) : 'home'
-$('[data-href="' + path + '"]').addClass('active')
-if ($('[data-href="' + path + '"]').parent()[0] === $('#robot-menu')[0]) $('#robot-menu').collapse('show')
+// Get path from URL params or home
+var path = URI(window.location.href).search(true).path ? unescape(URI(window.location.href).search(true).path) : 'home';
 
-load(path)
+// Set button as active and show dropdown if necessary
+$('[data-href="' + path + '"]').addClass('active');
+if ($('[data-href="' + path + '"]').parent()[0] === $('#robot-menu')[0]) $('#robot-menu').collapse('show');
+
+// Load the first page
+load(path);
--- js/style.js
+++ js/style.js
@@ -1,16 +1,20 @@
-var t = new URLSearchParams(window.location.search).get('theme');
-if (t !== null) {
-    var variant = t;
+// Use user-specified theme if it exists, otherwise dark mode (see below)
+var forceTheme = new URLSearchParams(window.location.search).get('theme');
+if (forceTheme !== null) {
+    var variant = forceTheme;
 } else {
     // var variant = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day';
-    var variant = 'night'; // TODO: change my mind
+    var variant = 'night'; // Dark mode is always right
+    // TODO: change my mind
 }
 
-var style = $('<link rel="stylesheet" href="/css/' + variant + '.css">')
+// Load correct CSS file and unhide body when done
+var style = $('<link rel="stylesheet" href="/css/' + variant + '.css">');
 style.on('load', _ => {
-    $('body').removeAttr('style')
-})
+    $('body').removeAttr('style');
+});
 
-$('head').append(style)
-$('head').append('<link rel="icon" href="/img/logos/icon-' + variant + '.png">')
-$('#brand').attr('src', '/img/logos/brand-' + variant + '.png')
+// Apply CSS and style logos accordingly
+$('head').append(style);
+$('head').append('<link rel="icon" href="/img/logos/icon-' + variant + '.png">');
+$('#brand').attr('src', '/img/logos/brand-' + variant + '.png');
