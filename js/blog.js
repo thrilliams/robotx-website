cc35adff79d67f504ad10696a43fffd5ceb48134
Soren <soren@aethre.co>
2020-02-05 17:38:50 -0800

    Put the code in the file!

--- js/blog.js
+++ js/blog.js
@@ -2,11 +2,27 @@ window.blogs = fetch('/get-blog-entries')
     .then(res => res.text())
     .then(body => JSON.parse(body))
 
+function loadBlogs(b) {
+    inject('')
+    b.forEach((blog, i) => {
+        inject(`<div class="jumbotron">
+            ${i === 0 ? '<br><br><br>' : ''}
+            <h3>${blog.title}</h3>
+            <p class="text-muted">${blog.sticky ? '<i class="fas fa-thumbtack"></i>   ' : ''}${new Date(blog.date).toDateString()}</p>
+            ${blog.body}
+            ${blog.authors && blog.authors.length > 0 ? `<p class="text-muted">Written by ${blog.authors.join(', ')}</p>` : ''}
+            ${blog.editors && blog.editors.length > 0 ? `<p class="text-muted">Edited by ${blog.editors.join(', ')}</p>` : ''}
+        </div>`, false)
+    })
+}
+
 /**
  * BlogEntry: {
  *   title: string,
  *   body: markdown | html,
  *   date: number,
- *   sticky: boolean
+ *   sticky: boolean,
+ *   authors: string[],
+ *   editors: string[]
  * }
  */
