cd9ffee7bf28063f368444b9b6d16607e2326200
Soren <soren@aethre.co>
2020-02-05 16:41:10 -0800

    Sticky posts

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,12 @@
 window.blogs = fetch('/get-blog-entries')
     .then(res => res.text())
     .then(body => JSON.parse(body))
+
+/**
+ * BlogEntry: {
+ *   title: string,
+ *   body: markdown | html,
+ *   date: number,
+ *   sticky: boolean
+ * }
+ */
